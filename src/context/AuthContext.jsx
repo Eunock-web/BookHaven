e8cc87486232baf6d8331a26891459a2da8ccfb7
Eunock <izumishinishi7@gmail.com>
2025-12-29 00:25:31 +0100

    fix: Implementation ou ajout de la gestion d'erreur global

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,6 +5,7 @@ const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null); //L'etat pour enregistrer les infos de l'utilisateur
+    const [errors, setErrors] = useState(null); //Etat pour centraliser les erreurs lors de l'authentification
 
     // Au démarrage, on regarde si une session existe
     useEffect(() => {
@@ -16,20 +17,31 @@ export const AuthProvider = ({ children }) => {
 
     // On enveloppe le login dans useCallback pour la performance
     const login = useCallback((email, password) => {
+        setErrors(null); //Initialisation de  l'etat a null a chaque appel de la fonction login
+        
         const result = Login(email, password);
         if (result.success) {
             setUser(result.data); // Mise à jour de l'état 
+        }else{
+            setErrors(result.response); //Renvoyer le message d'erreur
         }
+        
         return result;
     }, []);
 
     const logout = useCallback(() => {
-        Logout();
+        setErrors(null); //Initialisation de  l'etat a null a chaque appel de la fonction logout
+       const result =  Logout();
+       if(!result.success){
+            setErrors(result.response); //Renvoyer le message d'erreur
+       }else{
+        setErrors(result.response); 
         setUser(null); // L'interface se vide instantanément
+       }
     }, []);
 
     return (
-        <AuthContext.Provider value={{ user, login, logout }}>
+        <AuthContext.Provider value={{ user, login, logout, errors }}>
             {children}
         </AuthContext.Provider>
     );
