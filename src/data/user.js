2b8c70a618c60323cc176db914044b8bc05fe3b1
Eunock <izumishinishi7@gmail.com>
2025-12-29 00:41:34 +0100

    fix:Ajout des 09 utilisateurs restant

--- src/data/user.js
+++ src/data/user.js
@@ -5,6 +5,78 @@ export const user = [
         username : "Light",
         email : "erwinmith@gmail.com",
         role : 'admin',
+        password : "gogoislifes"
+    },
+
+    {
+        id: 2,
+        username : "Kagami",
+        email : "kagami@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id:3,
+        username : "YAGAMI",
+        email : "yagami@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 4,
+        username : "SENJOU",
+        email : "senjou@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 5,
+        username : "GOGO",
+        email : "gogo@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 6,
+        username : "Senpai",
+        email : "senpai@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 7,
+        username : "Light",
+        email : "erwinmith@gmail.com",
+        role : 'admin',
+        password : "gogoislife"
+    },
+
+    {
+        id: 8,
+        username : "Youga",
+        email : "youga@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 9,
+        username : "Tepei",
+        email : "tepei@gmail.com",
+        role : 'user',
+        password : "gogoislife"
+    },
+
+    {
+        id: 10,
+        username : "Koruko",
+        email : "koruko@gmail.com",
+        role : 'user',
         password : "gogoislife"
     }
 ]
