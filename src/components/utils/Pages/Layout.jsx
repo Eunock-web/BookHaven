4b2d18b22edf25bd1136b1d6d9c7a7339255c678
Eunock <izumishinishi7@gmail.com>
2025-12-30 02:41:01 +0100

    Correction

--- src/components/utils/Pages/Layout.jsx
+++ src/components/utils/Pages/Layout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom"
 import NavBar from "../components/NavBar"
+import Footer from "../components/Footer"
 
 
 function Layout(){
@@ -7,6 +8,7 @@ function Layout(){
         <div>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     </>
 }
